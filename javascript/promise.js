93cb68cc1800ee5e6d9ba1f93d38330ab4defdfd
bomangoo <mocona000@naver.com>
2020-07-29 00:22:47 +0900

    자바스크립트 기초강의 예제 Promise

---
+++ javascript/promise.js
@@ -0,0 +1,47 @@
+//콜백 지옥 탈출 promise 시작!
+
+class UserStorage{
+  loginUser(id, password, onSuccess, onError){
+    setTimeout(() => {
+      if(
+        (id === 'bomi' && password === 'army') ||
+        (id === 'coder' && password === 'job')
+      ){
+        onSuccess(id);
+      } else {
+        onError(new Error('not found'));
+      }
+    }, 2000);
+  }
+  getRoles(user, onSuccess, onError){
+    setTimeout(() => {
+      if(user === 'bomi'){
+        onSuccess({name: 'bomi', role: 'admin'});
+      } else {
+        onError(new Error('no access'));
+      }
+    }, 1000);
+  }
+}
+
+const userStorage = new UserStorage();
+const id = prompt('enter your id');
+const password = prompt('enter your password');
+userStorage.loginUser(
+  id,
+  password,
+  user => {
+    userStorage.getRoles(
+      user,
+      userWithRole => {
+        alert(`Hello ${userWithRole.name}, you have a ${userWithRole.role} role`)
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  },
+  error => {
+    console.log(error);
+  }
+)
