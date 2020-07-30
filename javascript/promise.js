6bd8b7febf5ff022c40229dd6ab3538099489fbc
bomangoo <mocona000@naver.com>
2020-07-30 22:52:55 +0900

    자바스크립트 기초강의 콜백함수 탈출 Promise

--- javascript/promise.js
+++ javascript/promise.js
@@ -1,47 +1,38 @@
 //콜백 지옥 탈출 promise 시작!
 
 class UserStorage{
-  loginUser(id, password, onSuccess, onError){
-    setTimeout(() => {
-      if(
-        (id === 'bomi' && password === 'army') ||
-        (id === 'coder' && password === 'job')
-      ){
-        onSuccess(id);
-      } else {
-        onError(new Error('not found'));
-      }
-    }, 2000);
+  loginUser(id, password){
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if(
+          (id === 'bomi' && password === 'army') ||
+          (id === 'coder' && password === 'job')
+        ){
+          resolve(id);
+        } else {
+          reject(new Error('not found'));
+        }
+      }, 2000);
+    })
   }
-  getRoles(user, onSuccess, onError){
-    setTimeout(() => {
-      if(user === 'bomi'){
-        onSuccess({name: 'bomi', role: 'admin'});
-      } else {
-        onError(new Error('no access'));
-      }
-    }, 1000);
+  getRoles(user){
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if(user === 'bomi'){
+          resolve({name: 'bomi', role: 'admin'});
+        } else {
+          reject(new Error('no access'));
+        }
+      }, 1000);
+    })
   }
 }
 
 const userStorage = new UserStorage();
 const id = prompt('enter your id');
 const password = prompt('enter your password');
-userStorage.loginUser(
-  id,
-  password,
-  user => {
-    userStorage.getRoles(
-      user,
-      userWithRole => {
-        alert(`Hello ${userWithRole.name}, you have a ${userWithRole.role} role`)
-      },
-      error => {
-        console.log(error);
-      }
-    );
-  },
-  error => {
-    console.log(error);
-  }
-)
+userStorage
+  .loginUser(id, password)
+  .then(userStorage.getRoles)
+  .then(user => alert(`Hello ${user.name}, you have a ${user.role} role`))
+  .catch(console.log)
